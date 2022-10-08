95143bb0c3cc49ff52334999633de3c9121f6780
Mark Abuyabor <mark@zeguestlist.com>
2022-10-08 10:21:28 +0800

    working wth fetch api

--- src/context.js
+++ src/context.js
@@ -1,9 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+const randomMealUrl = 'https:/www.themealdb.com/api/json/v1/1/random.php'
+
+
+
 const  AppProvider = ({children}) => {
-    return <AppContext.Provider value="hello">
+
+    useEffect(() => {
+        //console.log('fetch data here');
+        // fetch 
+        // fetch('https://randomuser.me/api/')
+        //     .then((response) => response.json())
+        //     .then((data) => console.log(data.results));
+
+
+        const fetchMeal = async (url) => {
+            
+            // axios.get(allMealsUrl)
+            //     .then(data => {
+            //         console.log(data.data);
+            //     })
+
+            try {
+                const {data} = await axios(url);
+                console.log(data);
+            } catch (error) {
+                console.log(error.response);
+            }
+
+
+
+            // try {
+            //     const response = await fetch('https://randomuser.me/api/');
+            //     const data = await response.json();
+            //     console.log(data);
+            // } catch(error) {
+            //     console.log(error);
+            // }
+        }
+
+        fetchMeal(allMealsUrl);
+
+    }, []);
+    
+    return <AppContext.Provider value="">
         {children}
     </AppContext.Provider>
 }
