17afc78a5e39e652dee40eff8c1786a31deda630
Mark Abuyabor <mark@zeguestlist.com>
2022-10-09 18:07:05 +0800

    fixing the error

--- src/context.js
+++ src/context.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const AppContext = React.createContext();
 
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
-const randomMealUrl = 'https:/www.themealdb.com/api/json/v1/1/random.php/'
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const  AppProvider = ({children}) => {
 
